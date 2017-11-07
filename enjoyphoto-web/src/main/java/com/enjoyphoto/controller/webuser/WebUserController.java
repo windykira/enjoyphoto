9cf7dd5b02e59f1eaa94d1838ead53b9f0cd312e
windy <windylucky@yeah.net>
2017-11-07 22:35:11 +0800

    rest

--- enjoyphoto-web/src/main/java/com/enjoyphoto/controller/webuser/WebUserController.java
+++ enjoyphoto-web/src/main/java/com/enjoyphoto/controller/webuser/WebUserController.java
@@ -3,12 +3,11 @@ package com.enjoyphoto.controller.webuser;
 import com.enjoyphoto.entity.webuser.WebUserEntity;
 import com.enjoyphoto.serivce.WebUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-<<<<<<< HEAD
-=======
 import org.springframework.web.bind.annotation.RequestMethod;
->>>>>>> a1ad3be3a4626bfd0d2d02e19891362de98a0703
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -46,18 +45,12 @@ public class WebUserController {
         return mav;
     }
 
-<<<<<<< HEAD
-    @RequestMapping("/user")
-    @ResponseBody
-    public ModelAndView createUser(HttpServletRequest request) throws IOException
-=======
-    @RequestMapping(value = "/user",method = RequestMethod.POST)
+    /*@RequestMapping(value = "/user",method = RequestMethod.PUT)
     @ResponseBody
     public WebUserEntity createUser(HttpServletRequest request, WebUserEntity webUserEntity) throws IOException
->>>>>>> a1ad3be3a4626bfd0d2d02e19891362de98a0703
     {
         ModelAndView mav = new ModelAndView();
-        /*WebUserEntity webUserEntity = new WebUserEntity();
+        *//*WebUserEntity webUserEntity = new WebUserEntity();
         webUserEntity.setLoginName("test111");
         webUserEntity.setPassWord("123456");
         webUserEntity.setEmail("xxx@163.com");
@@ -66,7 +59,7 @@ public class WebUserController {
         webUserEntity.setUserName("测试");
         webUserEntity.setCreateDate(new Date());
         webUserEntity.setUpdateDate(new Date());
-        webUserService.createWebUser(webUserEntity);*/
+        webUserService.createWebUser(webUserEntity);*//*
         mav.setViewName("success");
         //HttpSession session = request.getSession();
         //session.setAttribute("name", customer.getName());
@@ -74,14 +67,14 @@ public class WebUserController {
         webUserEntity.setUpdateDate(new Date());
         webUserService.createWebUser(webUserEntity);
         return webUserEntity;
-    }
+    }*/
 
-    @RequestMapping(value = "/user",method = RequestMethod.GET)
+    @RequestMapping(value = "/user/{userId:\\d+}",method = RequestMethod.GET)
     @ResponseBody
-    public WebUserEntity getWebUserById(HttpServletRequest request) throws IOException
+    public WebUserEntity getWebUserById(@PathVariable("userId") long userId) throws IOException
     {
         String str = "";
-        WebUserEntity webUserEntity = webUserService.getWebUserById(Long.valueOf(request.getParameter("userId")));
+        WebUserEntity webUserEntity = webUserService.getWebUserById(userId);
         return webUserEntity;
     }
 }
--- enjoyphoto-web/src/main/webapp/WEB-INF/js/data/data.js
+++ enjoyphoto-web/src/main/webapp/WEB-INF/js/data/data.js
@@ -32,17 +32,11 @@ define(function(require,exports,module){
             url: "/webUser/user",
             //data: {loginName:$("#loginName").val()},
             data: {userId:1},
+            contentType: "application/json;charset=utf-8",
             dataType: "json",
 
             success: function(data){
                 var s = '';
-                /*$('#resText').empty();   //清空resText里面的所有内容
-                 var html = '';
-                 $.each(data, function(commentIndex, comment){
-                 html += '<div class="comment"><h6>' + comment['username']
-                 + ':</h6><p class="para"' + comment['content']
-                 });
-                 $('#resText').html(html);*/
             }
 
         });
