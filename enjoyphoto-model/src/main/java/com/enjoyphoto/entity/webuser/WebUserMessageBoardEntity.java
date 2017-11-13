3061bcdda6af89dce4fc9cf109e6d44f493a8be2
windy <windylucky@yeah.net>
2017-11-13 22:16:36 +0800

    enjoy

--- enjoyphoto-model/src/main/java/com/enjoyphoto/entity/webuser/WebUserMessageBoardEntity.java
+++ enjoyphoto-model/src/main/java/com/enjoyphoto/entity/webuser/WebUserMessageBoardEntity.java
@@ -1,14 +1,20 @@
 package com.enjoyphoto.entity.webuser;
 
 import com.enjoyphoto.entity.base.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Created by Administrator on 2017/11/13.
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class WebUserMessageBoardEntity extends BaseEntity{
 
     private long userId;
     private long messageUserId;
     private String content;
-    
+
 }
