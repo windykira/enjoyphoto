8151888707ef17f5f328b302d16ef7d7fd910617
windy <windylucky@yeah.net>
2017-11-23 21:36:34 +0800

    enjoy

---
+++ enjoyphoto-web/src/main/java/com/enjoyphoto/TestMain.java
@@ -0,0 +1,16 @@
+package com.enjoyphoto;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Created by Administrator on 2017/11/23.
+ */
+public class TestMain {
+
+
+    static Logger log = Logger.getLogger("myTest");
+    public static void main(String[] args){
+        log.info("test");
+    }
+
+}
