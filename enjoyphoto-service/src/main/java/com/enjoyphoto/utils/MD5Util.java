ad689e08a6420ac7601fac9587d245425e221896
windy <windylucky@yeah.net>
2017-12-06 21:57:00 +0800

    enjoy

--- enjoyphoto-service/src/main/java/com/enjoyphoto/utils/MD5Util.java
+++ enjoyphoto-service/src/main/java/com/enjoyphoto/utils/MD5Util.java
@@ -20,7 +20,6 @@ public class MD5Util {
     public static String EncoderByMd5(String str, String alt) throws NoSuchAlgorithmException,
             UnsupportedEncodingException {
 
-        //确定计算方法
         MessageDigest md5 = MessageDigest.getInstance("MD5");
         BASE64Encoder base64en = new BASE64Encoder();
         str += alt;
