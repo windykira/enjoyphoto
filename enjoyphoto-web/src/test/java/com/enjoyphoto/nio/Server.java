d1a971d71253b4d0495f5763172f4c2d6af374a0
马 小磊 <maxl@baiten.cn>
2018-03-06 17:07:09 +0800

    nio

---
+++ enjoyphoto-web/src/test/java/com/enjoyphoto/nio/Server.java
@@ -0,0 +1,23 @@
+package com.enjoyphoto.nio;
+
+/**
+ * Created by windy on 2018/3/6.
+ */
+public class Server {
+
+    private static int DEFAULT_PORT = 12345;
+    private static ServerHandle serverHandle;
+
+    public static void start(){
+        start(DEFAULT_PORT);
+    }
+    public static synchronized void start(int port){
+        if(serverHandle!=null)
+            serverHandle.stop();
+        serverHandle = new ServerHandle(port);
+        new Thread(serverHandle,"Server").start();
+    }
+    public static void main(String[] args){
+        start();
+    }
+}
---
+++ enjoyphoto-web/src/test/java/com/enjoyphoto/nio/TestMain.java
@@ -0,0 +1,22 @@
+package com.enjoyphoto.nio;
+
+import java.util.Scanner;
+
+/**
+ * Created by windy on 2018/3/6.
+ */
+public class TestMain {
+
+    //测试主方法
+    @SuppressWarnings("resource")
+    public static void main(String[] args) throws Exception{
+        //运行服务器
+        Server.start();
+        //避免客户端先于服务器启动前执行代码
+        Thread.sleep(100);
+        //运行客户端
+        Client.start();
+
+        while(Client.sendMsg(new Scanner(System.in).nextLine()));
+    }
+}
